2cecb5bc8a452b8d539f42c3c5da0d43724f53c6
blackwebZen <blackweb.zen@gmail.com>
2020-01-18 12:41:44 -0800

    Ready 2 Begin

--- MedApp/settings.py
+++ MedApp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'y9+rp^4mwqx_pizwy(e#b0mvd%u*)6i8*$j4@oabtd@x1$!a#y'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'MedWebApp'
 ]
 
 MIDDLEWARE = [
@@ -119,3 +120,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'MedWebApp/static/') ## correct path for deployment setting
--- MedWebApp/views.py
+++ MedWebApp/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
-
+from django.views import View
+from django.http import HttpResponse, JsonResponse
+from django.template.loader import render_to_string
+from django.core.mail import send_mail
 # Create your views here.
+
+
+class Index(View):
+    def get(self,request):
+        return render(request, "MedWebApp/index.html", context=None)
+
+    def post(self,request):
+        pass
